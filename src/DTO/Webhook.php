c910f881aef44d07b65f851692b8f700655f0854
valerii <vsierhieiev-its24@lll.kpi.ua>
2025-03-09 18:38:04 +0200

    feat: add webhook dto

---
+++ src/DTO/Webhook.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\DTO;
+
+class Webhook
+{
+    private string $event;
+
+    private string $url;
+
+    public function getEvent(): string
+    {
+        return $this->event;
+    }
+
+    public function setEvent(string $event): void
+    {
+        $this->event = $event;
+    }
+
+    public function getUrl(): string
+    {
+        return $this->url;
+    }
+
+    public function setUrl(string $url): void
+    {
+        $this->url = $url;
+    }
+
+
+}
